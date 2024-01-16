6bfb265851498d12af81192f27d3bb78fb9642bc
chipchippp <nvl150424@gmail.com>
2024-01-17 00:16:49 +0700

    update

---
+++ ContosoUniversity/Migrations/20240116164903_AddFullNameProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullNameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "FirstName");
+        }
+        
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Student",
+                newName: "FirstMidName");
+        }
+    }
+}
